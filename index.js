e5c5956e9b17e54a0214b6c01db54a2fa0536182
botcarola <celerner4@gmail.com>
2022-11-16 13:02:53 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+// DOM
+const contenedorProductos = document.querySelector(".productos")
+
+
+// Carrousel
+const swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
