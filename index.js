3990784bfc3a79854a31a5ac96489b9883d8fe11
botcarola <celerner4@gmail.com>
2022-11-22 16:39:08 -0300

    añado navegación entre apartados

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
 // DOM
-const contenedorProductos = document.querySelector(".productos")
+const apartadoHome = document.querySelector(".home")
+const apartadoProductos = document.querySelector(".productos")
+const apartadoCarrito = document.querySelector(".carrito")
+const apartadoContacto = document.querySelector(".contacto")
 const contenedorDestacados = document.querySelector(".productos-destacados")
 const resultadosDestacados = document.querySelector (".subtitle-resultados")
+const navHome = document.querySelector("#home-nav")
+const navProductos = document.querySelector("#products-nav")
+const navEmptyCart = document.querySelector("#empty-cart-nav")
+const navFillCart = document.querySelector("#cart-fill-nav")
+const navContacto = document.querySelector("#contact-nav")
+
+
+// Navegación
+
+const navegacion = ( trigger, visible , oculto1, oculto2, oculto3  ) => {
+    
+    trigger.onclick = () => {
+
+        visible.style.display = "block"
+        oculto1.style.display = "none"
+        oculto2.style.display = "none"
+        oculto3.style.display = "none"
+    }
+}
+
+navegacion( navHome, apartadoHome, apartadoProductos, apartadoCarrito, apartadoContacto )
+navegacion( navProductos, apartadoProductos, apartadoHome, apartadoCarrito, apartadoContacto )
+navegacion( navEmptyCart, apartadoCarrito, apartadoHome, apartadoProductos, apartadoContacto)
+navegacion( navFillCart, apartadoCarrito, apartadoHome, apartadoProductos, apartadoContacto)
+navegacion( navContacto, apartadoContacto, apartadoHome, apartadoProductos, apartadoCarrito )
 
 
 // Carrousel
@@ -36,13 +64,21 @@ const destacadosHtml =  array => {
                         <h2>
                             ${curr.producto}
                         </h2>
-                        <h3>
-                            ${curr.destacado === true && "30% OFF" }
-                        </h3>
+                        ${ 
+                            curr.destacado === true && 
+                                `<h3>
+                                    30% OFF
+                                </h3>`
+                        }
                     </div>
-                    <h3 class="precio-producto">
-                        $${curr.destacado === true ? curr.precio - ((curr.precio * 30) / 100) : curr.precio}
-                    </h3>
+                    <div>
+                        <h3 class="precio-producto">
+                            $${curr.destacado === true ? curr.precio - ((curr.precio * 30) / 100) : curr.precio}
+                        </h3>
+                        <button class="agregar-carrito">
+                            Añadir al carrito
+                        </button>
+                    </div>
                 </div>
             </article>
         `
